a8b7d1630ff856dfa9b4438f508f1172cbfbda72
root <mail@pull-phone.com>
2014-10-28 19:09:17 +0900

    fix docker bin

--- mysql_master/docker_setting.sh
+++ mysql_master/docker_setting.sh
@@ -1,4 +1,5 @@
-DOCKER_BIN_NAME="docker.io"
+#!/bin/bash
+DOCKER_BIN_NAME="docker"
 DOCKER_MYSQL_DIR="/home/mysql/master"
 DOCKER_IMAGE_NAME="pull/mysql-master"
 DOCKER_CONTAINER_NAME="mysql-master"
