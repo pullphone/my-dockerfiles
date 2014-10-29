fc6952df394dd8338d48f8fa9e363a6c7c9d4430
pull <mail@pull-phone.com>
2014-10-29 19:59:54 +0900

    divide nginx and php-fpm dockerfiles

--- nginx/docker_setting.sh
+++ nginx/docker_setting.sh
@@ -1,5 +1,5 @@
 DOCKER_BIN_NAME="docker"
-DOCKER_IMAGE_NAME="pull/frontend"
-DOCKER_CONTAINER_NAME="frontend"
+DOCKER_IMAGE_NAME="pull/nginx"
+DOCKER_CONTAINER_NAME="nginx"
 DOCKER_WWW_DIR="/home/www/experiment.pull-phone.com"
 DOCKER_NGINX_LOG_DIR="/home/www/nginx_logs"
---
+++ php-fpm/docker_setting.sh
@@ -0,0 +1,4 @@
+DOCKER_BIN_NAME="docker"
+DOCKER_IMAGE_NAME="pull/php-fpm"
+DOCKER_CONTAINER_NAME="php-fpm"
+DOCKER_WWW_DIR="/home/www/experiment.pull-phone.com"
